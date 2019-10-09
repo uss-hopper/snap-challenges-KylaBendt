dba52911a9d41b35fd047c6a2adbdac6f287b04b
Kyla Bendt <kylabendt@gmail.com>
2019-10-09 08:52:07 -0600

    added comments and cleaned up code

--- functions.js
+++
@@ -1,5 +0,0 @@
-let numberOne = 1;
-let numberTwo = 2;
-
-let newNumber = numberOne * numberTwo;
-console.log(newNumber);
--- snap-10-9-functions/functions.js
+++ snap-10-9-functions/functions.js
@@ -1,21 +1,34 @@
 let numberOne = 1;
 let numberTwo = 2;
+let numberThree = multiplyTwoNumbers(numberOne, numberTwo);
+console.log("New Number: " + numberThree);
+
+let stringOne = "Good";
+let stringTwo = "Morning";
+let stringThree = smushStrings(stringOne, stringTwo);
+console.log("New String: " + stringThree);
+
+
 /*
 * Returns the product of two numbers
 *
+* @Param $num1 first number to be multiplied
+* @Param $num1 second number to be multiplied
+* @return $product value of num1 * num2
 * */
 function multiplyTwoNumbers(num1, num2) {
 	let product = num1 * num2;
 	return product;
 }
 
-console.log("New Number:" + multiplyTwoNumbers(numberOne, numberTwo));
 
-let stringOne = "Good";
-let stringTwo = "Morning";
-let stringThree = smushStrings(stringOne, stringTwo);
-console.log("New String:" + stringThree);
-
+/*
+* Concatenates two strings
+*
+* @Param $str1 first string to be concatenated
+* @Param $str1 second string to be concatenated
+* @return $product str1 and str2 concatenated
+* */
 function smushStrings(str1, str2) {
 	let newString = str1 + " " + str2;
 	return newString;
