d3b030cb3e725298f1d1be57f8dd9ea00f99d883
Kyla Bendt <kylabendt@gmail.com>
2019-10-11 08:31:30 -0600

    create files for octoebr 11 snap

---
+++ snap-10-11-data-design/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Snap October 11 - Data Design</title>
+
+</head>
+<body>
+<h1>Kyla Snap Challenge October 11 - User Stories and Entities</h1>
+<a href="./q1-user-story.php">Question 1: User Story</a>
+<a href="./q2-entities.php">Question 2: Entities</a>
+
+</body>
+
+</html>
