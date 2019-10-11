5fa9c673dcbd9bce54d0e1e640e47c447946bc29
Kyla Bendt <kylabendt@gmail.com>
2019-10-11 08:40:53 -0600

    add user story to q1

--- snap-10-11-data-design/index.php
+++ snap-10-11-data-design/index.php
@@ -7,8 +7,8 @@
 </head>
 <body>
 <h1>Kyla Snap Challenge October 11 - User Stories and Entities</h1>
-<a href="./q1-user-story.php">Question 1: User Story</a>
-<a href="./q2-entities.php">Question 2: Entities</a>
+<p><a href="./q1-user-story.php">Question 1: User Story</a></p>
+<p><a href="./q2-entities.php">Question 2: Entities</a></p>
 
 </body>
 
--- snap-10-11-data-design/q1-user-story.php
+++ snap-10-11-data-design/q1-user-story.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Question 1: User Story</title>
+
+</head>
+<body>
+<h1>Question 1: User Story</h1>
+<p>As a registered user I want to add text to one of my images.</p>
+
+</body>
+</html>
