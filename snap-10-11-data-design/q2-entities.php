1706048d4bf5f34fd45d608b8845074b23e858fa
Kyla Bendt <kylabendt@gmail.com>
2019-10-11 10:37:02 -0600

    Added note to ERD page to mention typos

--- snap-10-11-data-design/q2-entities.php
+++ snap-10-11-data-design/q2-entities.php
@@ -6,5 +6,6 @@
 </head>
 <h1>Question 2: ERD</h1>
 <img src="./images/erd.jpg" alt="Entity Relationship Diagram">
+<p>*Under UserAddress, addressId should be userAddressAddressId and userId should be userAddressUserId*</p>
 
 </html>
