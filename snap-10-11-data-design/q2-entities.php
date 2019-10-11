79065879968a54f59c17576779c9c331b23bb5bc
Kyla Bendt <kylabendt@gmail.com>
2019-10-11 09:08:36 -0600

    added erd to q2

--- snap-10-11-data-design/q2-entities.php
+++ snap-10-11-data-design/q2-entities.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Question 2: ERD</title>
+</head>
+<h1>Question 2: ERD</h1>
+<img src="./images/erd.jpg" alt="Entity Relationship Diagram">
+
+</html>
