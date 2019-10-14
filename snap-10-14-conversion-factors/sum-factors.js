33f4edf6eaf86aace93909cad9837f0c16bfa24b
Kyla Bendt <kylabendt@gmail.com>
2019-10-14 08:54:28 -0600

    add function to sum factors

--- snap-10-14-conversion-factors/sum-factors.js
+++ snap-10-14-conversion-factors/sum-factors.js
@@ -1,6 +1,6 @@
 /*This code is designed to take a positive integer, find all positive factors and sum them. */
 window.onload = function() {
-
+ console.log (sumFactors(12));
 };
 
 //Set event listener to get number from html
@@ -21,7 +21,9 @@ function handleNumberForm(event, formRef) {
 	formRef.reset();
 }
 
-function displayFactorSum();
+function displayFactorSum(){
+
+}
 //TODO: Check input to make sure it is a positive integer
 //TODO: If it a positive integer, proceed w/ function
 //TODO: If it is not a positive integer, give error
@@ -37,5 +39,14 @@ TODO: For each number, check if it is a factor (mod = 0)
 TODO: if it is a factor, add to running sum
 TODO: return sum
    */
-function sumFactors(number);
-for
+function sumFactors(number) {
+	let sum = 0;
+	for(let i=1;i <= number;i++) {
+		if((number % i) === 0) {
+			sum = sum + i;
+		}
+	}
+	return sum;
+}
+
+
