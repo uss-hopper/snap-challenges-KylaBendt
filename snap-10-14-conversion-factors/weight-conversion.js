ff2d12b7e45e147a7538e58024446ef627be9181
Kyla Bendt <kylabendt@gmail.com>
2019-10-14 09:24:49 -0600

    add outline for convertWeight function

---
+++ snap-10-14-conversion-factors/weight-conversion.js
@@ -0,0 +1,15 @@
+
+
+
+
+/*
+* Convert a weight or mass to grams.
+*
+* @param weight The weight or mass to be converted.
+* @param units The abbreviation for the units in which the weight/mass is measured
+* @returns The mass in grams
+* */
+function convertWeight(weight, units) {
+
+	return grams;
+}
