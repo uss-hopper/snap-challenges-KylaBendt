a62c58fad9a056bfb3348bada1d6ba4bd9f7ce3d
Kyla Bendt <kylabendt@gmail.com>
2019-10-14 08:24:09 -0600

    Add basic html to sum-factors.php

--- snap-10-14-conversion-factors/sum-factors.php
+++ snap-10-14-conversion-factors/sum-factors.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sum factors</title>
+	<script src="./sum-factors.js"></script>
+</head>
+<h1>Find the sum of all factors of a number</h1>
+<!--An input number thing will go here-->
+<!--A place to output the answer will go here-->
+
+
+</html>
