981a15c0da770b8e4bee0fe7001eb1c251e05465
Kyla Bendt <kylabendt@gmail.com>
2019-10-14 08:09:35 -0600

    setup 10-14 files

---
+++ snap-10-14-conversion-factors/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>October 14th</title>
+</head>
+
+<body>
+<h1>October 14th Challenges</h1>
+<a href="../snap-10-14-conversion-factors/weight-conversion.php">Weight Conversion</a>
+<a href="../snap-10-14-conversion-factors/sum-factors.php">Sum factors of a number</a>
+
+
+</body>
+
+</html>
