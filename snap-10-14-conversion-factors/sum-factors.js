14f5a5be4c4241a37849995e37ccf78155879ebd
Kyla Bendt <kylabendt@gmail.com>
2019-10-14 08:19:17 -0600

    add psuedocode outline to sum-factors.js

---
+++ snap-10-14-conversion-factors/sum-factors.js
@@ -0,0 +1,19 @@
+/*This code is designed to take a positive integer, find all positive factors and sum them. */
+
+//TODO: Get number from html
+
+//TODO: Check input to make sure it is a positive integer
+//TODO: If it a positive integer, proceed w/ function
+//TODO: If it is not a positive integer, give error
+
+//TODO: Return result to html
+
+
+
+/*
+
+TODO: Write a for loop that cycles through all numbers from 1 to input
+TODO: For each number, check if it is a factor (mod = 0)
+TODO: if it is a factor, add to running sum
+TODO: return sum
+   */
