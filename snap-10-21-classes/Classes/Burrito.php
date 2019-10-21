1e5d235bd6a8053f53030f4a13638f908f85cb02
Kyla Bendt <kylabendt@gmail.com>
2019-10-21 08:41:59 -0600

    fill in Burrito class, add   instance

--- snap-10-21-classes/Classes/Burrito.php
+++ snap-10-21-classes/Classes/Burrito.php
@@ -12,5 +12,106 @@ namespace KylaBendt\SnapChallengesKylaBendt;
  */
 class Burrito {
 
+//Define state variables
+/**
+ * chile color - red or green!
+ * @var string $chileColor
+ */
+private $burritoChileColor;
+
+/*
+ * meatType
+ * @var string $meatType
+ */
+private $burritoMeatType;
+
+/*
+ * Constructor for this burrito
+ *
+ * @param string $newBurritoChileColor chile color of this burrito- must be 'red' or 'green'
+ * @param string $newBurritoMeatType meat type of this burrito
+ */
+
+public function __construct($newBurritoChileColor, $newBurritoMeatType) {
+	try {
+		$this->setBurritoChileColor($newBurritoChileColor);
+		$this->setBurritoMeatType($newBurritoMeatType);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception->getMessage(), 101, $exception));
+	}
+}
+
+/*
+ * accessor for burritoChileColor
+ *
+ * @return string value of burritoChileColor
+ */
+
+public function getBurritoChileColor(): string {
+	return ($this->burritoChileColor);
+}
+
+/*
+ * mutator for burritoChileColor
+ *
+ * @param string 'red' or 'green' $newBurritoChileColor accepts
+ * @throws \InvalidArgumentException if $newBurritoChileColor is not 'red' or 'green'
+ * @throws \TypeError if $newBurritoChileColor is not a string
+ */
+
+public function setBurritoChileColor($newBurritoChileColor): void {
+	//Sanitize the input
+	$newBurritoChileColor = trim($newBurritoChileColor);
+	$newBurritoChileColor = filter_var($newBurritoChileColor, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(empty($newBurritoChileColor) === true) {
+		throw(new \InvalidArgumentException("newBurritoChileColor is empty or insecure"));
+	}
+
+	//Check if chile type is 'red' or 'green'
+	if($newBurritoChileColor != 'red' | $newBurritoChileColor != 'green') {
+		throw(new \InvalidArgumentException("chile type must be 'red' or 'green'"));
+	}
+
+	//store the chile type
+	$this->burritoChileColor = $newBurritoChileColor;
+}
+
+
+/*
+ * accessor for burritoMeatType
+ *
+ * @return string value of burritoMeatType
+ */
+
+public function getBurritoMeatType(): string {
+	return ($this->burritoMeatType);
+}
+
+
+/*
+ * mutator for burritoMeatType
+ *
+ * @param string  $newBurritoMeatType
+ * @throws \InvalidArgumentException if input is not secure
+ * @throws \TypeError if $newBurritoMeatType is not a string
+ */
+
+public function setMeatType($newBurritoMeatType): void {
+	//Sanitize the input
+	$newBurritoMeatType = trim($newBurritoMeatType);
+	$newBurritoMeatType = filter_var($newBurritoMeatType, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(empty($newBurritoMeatType) === true) {
+		throw(new \InvalidArgumentException("newBurritoMeatType is empty or insecure- no vegetarians allowed!!!"));
+	}
+
+
+	//store the meat type
+	$this->burritoMeatType = $newBurritoMeatType;
+}
+
+public static function eatBurrito() : void {
+	echo("Your burrito is gone!");
+}
 
 }
--- snap-10-21-classes/lib/burrito.php
+++ snap-10-21-classes/lib/burrito.php
@@ -1,1 +1,10 @@
 <?php
+require_once("../Classes/Burrito.php");
+use KylaBendt\SnapChallengesKylaBendt\Burrito;
+
+$myBurrito = new Burrito('green', 'bacon');
+
+echo("My burrito has ");
+echo($myBurrito->getBurritoChileColor());
+echo(" chile and ");
+echo($myBurrito->getBurritoMeatType());
