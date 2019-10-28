eec1bbcb33ae5ecc212095e9d82f6e337a80a173
Kyla Bendt <kylabendt@gmail.com>
2019-10-28 08:13:41 -0600

    add documentation

--- snap-10-28-getFooByBar/getTweetsByBar.php
+++ snap-10-28-getFooByBar/getTweetsByBar.php
@@ -1,1 +1,10 @@
 <?php
+
+/**
+ * gets tweets by tweet date
+ *
+ * @param \PDO $pdo PDO connection object
+ * @returns \SplFixedArray of tweets found or null if not found
+ * @throws \PDOException when SQL related errors occur
+ * @throws \TypeError when variables are not of the correct type
+ * */
