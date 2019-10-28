ccaf610f152eb8bdda803bed3876354940abba35
Kyla Bendt <kylabendt@gmail.com>
2019-10-28 08:17:43 -0600

    function skeleton

--- snap-10-28-getFooByBar/getTweetsByTweetDate.php
+++ snap-10-28-getFooByBar/getTweetsByTweetDate.php
@@ -4,7 +4,17 @@
  * gets tweets by tweet date
  *
  * @param \PDO $pdo PDO connection object
+ * @param \DateTime $dateTime
  * @returns \SplFixedArray of tweets found or null if not found
  * @throws \PDOException when SQL related errors occur
  * @throws \TypeError when variables are not of the correct type
  * */
+
+public static function getTweetsByTweetDate(DateTime $dateTime, \PDO $pdo) : \SplFixedArray {
+	//create query template
+
+	//execute query
+
+	//build an array of tweets
+
+}
