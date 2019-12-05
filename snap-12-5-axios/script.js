cac9ec526e508196c6991edf822867fbbad809aa
Kyla Bendt <kylabendt@gmail.com>
2019-12-05 08:41:39 -0700

    put some dogs in div

--- snap-12-5-axios/script.js
+++ snap-12-5-axios/script.js
@@ -1,3 +1,9 @@
 function onPageLoad() {
 
+		// axios.get('https://dog.ceo/api/breeds/list/all').then(result => {
+		// 	const breeds = result.data;
+		// });
+	let breeds = "dog, another dog";
+	let breedDiv = document.getElementById("target");
+	breedDiv.textContent = breeds;
 }
