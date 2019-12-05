bc64956255916251e58dade7dd70dbeb1869a8d5
Kyla Bendt <kylabendt@gmail.com>
2019-12-05 09:17:36 -0700

    change from display object keys to keys and values

--- snap-12-5-axios/script.js
+++ snap-12-5-axios/script.js
@@ -4,7 +4,11 @@ function onPageLoad() {
 		let breeds = result.data.message;
 		console.log(breeds);
 		let breedDiv = document.getElementById("target");
-		breedDiv.textContent = Object.keys(breeds);
+		breedDiv.textContent = "";
+		// breedDiv.textContent = Object.keys(breeds);
+		for (let [key, value] of Object.entries(breeds)) {
+			breedDiv.append(`${key}: ${value}`);
+		}
 	});
 
 }
