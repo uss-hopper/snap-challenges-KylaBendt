074ff5c64ecb5c39ae0101becda3ccbb19781587
Kyla Bendt <kylabendt@gmail.com>
2019-12-05 09:35:35 -0700

    remove console logs and clean up code

--- snap-12-5-axios/script.js
+++ snap-12-5-axios/script.js
@@ -1,17 +1,13 @@
 function onPageLoad() {
-
 	axios.get('https://dog.ceo/api/breeds/list/all').then(result => {
 		let breeds = result.data.message;
-		console.log(breeds);
 		let breedDiv = document.getElementById("target");
 		breedDiv.textContent = "";
-		// breedDiv.textContent = Object.keys(breeds);
 		for (let [key, value] of Object.entries(breeds)) {
 			let child = document.createElement("p");
 			child.textContent = `${key}: ${value}`;
 			breedDiv.appendChild(child);
 		}
 	});
-
 }
 
