015e9795e0872dc4e21ef75760fdc65dc268a53c
Kyla Bendt <kylabendt@gmail.com>
2019-12-05 09:12:16 -0700

    output a really, really, really long line of dogs.  Woof.

--- snap-12-5-axios/script.js
+++ snap-12-5-axios/script.js
@@ -1,9 +1,11 @@
 function onPageLoad() {
 
-		// axios.get('https://dog.ceo/api/breeds/list/all').then(result => {
-		// 	const breeds = result.data;
-		// });
-	let breeds = "dog, another dog";
-	let breedDiv = document.getElementById("target");
-	breedDiv.textContent = breeds;
+	axios.get('https://dog.ceo/api/breeds/list/all').then(result => {
+		let breeds = result.data.message;
+		console.log(breeds);
+		let breedDiv = document.getElementById("target");
+		breedDiv.textContent = Object.keys(breeds);
+	});
+
 }
+
